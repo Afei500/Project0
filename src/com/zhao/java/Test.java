77873324df8743c2dc2150bf6b6ee772c073d250
Afei500 <1179705488@qq.com>
2022-07-06 13:44:11 +0800

    Initial commit

---
+++ src/com/zhao/java/Test.java
@@ -0,0 +1,14 @@
+package com.zhao.java;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:这是一个java程序
+ * User: zhao
+ * Date: 2022-07-06
+ * Time: 11:56
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
